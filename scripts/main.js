02f5d532b6335d307a57cca6c750a8ae54dc8e89
tripzoredd <132893607+tripzoredd@users.noreply.github.com>
2023-05-30 14:31:07 +0300

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const body = document.body;
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    body.classList.toggle("noscroll");
-})
